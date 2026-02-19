03d9fefbffb4afac5e93a7ae7e36942043e5e190
Kaipauole <kaipauole@gmail.com>
2026-02-19 11:23:28 -0700

    16th commit

---
+++ Assignments/Assignment_2/Assignment_2.R
@@ -0,0 +1,16 @@
+list.files('Data/', pattern='.csv$', recursive=T)
+length(list.files('Data/', pattern = '.csv$', recursive =T))
+df <- read.csv('Data/wingspan_vs_mass.csv')
+head(df, 5)
+b_files <- list.files('Data/', pattern='^b', recursive=T)
+for (i in b_files) {
+  print(i)
+  first_line <- readLines(file.path('Data', i), n = 1)
+  print(first_line)
+}
+csv._files <- list.files('data/', pattern='.csv$', recursive=T)
+for (i in csv._files){
+  print(i)
+  other_first_line <- readLines(file.path('data',i ), n = 1)
+  print(other_first_line)
+}
